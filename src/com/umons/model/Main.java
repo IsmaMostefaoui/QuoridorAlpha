9821e334d2985275975ed35bb410f38418f0a507
isma <ismajesuis@live.fr>
2016-02-19 21:16:39 +0100

    Ajout de test deplacement

--- src/com/umons/model/Main.java
+++ src/com/umons/model/Main.java
@@ -6,15 +6,20 @@ public class Main {
 		Grid plateau ;
 		plateau = new Grid();
 		plateau.fillGrid();
-		Player joueur1 = new Player(1);
-		Player joueur2 = new Player(2);
+		Player joueur1 = new Player(1, plateau);
+		Player joueur2 = new Player(2, plateau);
 		System.out.println("**********Grille initiale :***********");
+		System.out.println();
 		plateau.afficheGrid(joueur1, joueur2);
+		System.out.println();
 		System.out.println("*******Deplacement de joueur 1********");
-		joueur1.getPawn().move("sud");
+		System.out.println();
+		joueur1.getPawn().move("sud", plateau);
 		plateau.afficheGrid(joueur1, joueur2);
+		System.out.println();
 		System.out.println("*******Deplacement de joueur 2********");
-		joueur2.getPawn().move("nord");
+		System.out.println();
+		joueur2.getPawn().move("nord", plateau);
 		plateau.afficheGrid(joueur1, joueur2);
 	}
 }
