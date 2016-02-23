8659e52bb948751639221080074dddd0eee57f0b
isma <ismajesuis@live.fr>
2016-02-23 19:32:12 +0100

    Plein de petite modif au niveau affichage, au niveau index du tableau, rajout d'un item, renommage des variables

--- src/com/umons/model/Player.java
+++ src/com/umons/model/Player.java
@@ -16,20 +16,20 @@ public class Player{
 		 **/
 		numberOfWall = 10;
 		//initialisation du pion
-		posX = 8;
+		posX = 9;
 		if (number == 1) {
-			posY = 16;
+			posY = 17;
 		}else if (number == 2){
-			posY = 0;
+			posY = 1;
 		}else {
 			//Si pas 1 ou 2, alors on le place au mileu
-			posY = 8;
+			posY = 9;
 		}
 		//connection avec la case
 		//Rempli la case du joueur 2
-		grid.setItemInGrid(0, 8, true);
+		grid.setItemInGrid(1, 9, true);
 		//Rempli la case du joueur 1
-		grid.setItemInGrid(16, 8, true);
+		grid.setItemInGrid(17, 9, true);
 	}
 	
 	public void putWall(Grid grid, String position, int x, int y){
@@ -58,38 +58,38 @@ public class Player{
 		//On verra ça plus tard, en gros, ce sont des enchainements de else if plus lisible
 		switch (direction) {
 		//permet d'enlever de la grille le pion et de le remmettre sur les nouvelles coordonees
-		case "nord":
-			if (Rules.rMove(this.posX, posY)) { //ici c'est x, y car la methode s occupe de l inversement ligne colonne
-				grid.setItemInGrid(this.posY, this.posX, false);
-				this.posY -= 2;
-				grid.setItemInGrid(this.posY, this.posX, true);
+		case "z":
+			if (Rules.rMove(posX, posY)) { //ici c'est x, y car la methode s occupe de l inversement ligne colonne
+				grid.setItemInGrid(posY, posX, false);
+				posY -= 2;
+				grid.setItemInGrid(posY, posX, true);
 				return true;
 			}else {
 				return false;
 			}
-		case "ouest":
-			if (Rules.rMove(this.posX, posY)){
-				grid.setItemInGrid(this.posY, this.posX, false);
-				this.posX -= 2;
-				grid.setItemInGrid(this.posY, this.posX, true);
+		case "q":
+			if (Rules.rMove(posX, posY)){
+				grid.setItemInGrid(posY, posX, false);
+				posX -= 2;
+				grid.setItemInGrid(posY, posX, true);
 				return true;
 			}else {
 				return false;
 			}
-		case "est":
-			if (Rules.rMove(this.posX, posY)) {
-				grid.setItemInGrid(this.posY, this.posX, false);
-				this.posX += 2;
-				grid.setItemInGrid(this.posY, this.posX, true);
+		case "d":
+			if (Rules.rMove(posX, posY)) {
+				grid.setItemInGrid(posY, posX, false);
+				posX += 2;
+				grid.setItemInGrid(posY, posX, true);
 				return true;
 			}else {
 				return false;
 			}
-		case "sud":
-			if (Rules.rMove(this.posX, posY)) {
-				grid.setItemInGrid(this.posY, this.posX, false);
-				this.posY += 2;
-				grid.setItemInGrid(this.posY, this.posX, true);
+		case "s":
+			if (Rules.rMove(posX, posY)) {
+				grid.setItemInGrid(posY, posX, false);
+				posY += 2;
+				grid.setItemInGrid(posY, posX, true);
 				return true;
 			}else {
 				return false;
@@ -108,7 +108,7 @@ public class Player{
 	}
 	
 	//Accesseur du pion
-	public int getY() {
+	public int getPawnY() {
 		/**
 		 * Accesseur
 		 * @return la position en y du Pion (LIGNE)
@@ -116,7 +116,7 @@ public class Player{
 		return posY;	
 	}
 	
-	public int getX(){
+	public int getPawnX(){
 		/**
 		 * Accesseur
 		 * @return la position du Pion (COLONNE)
