d49d60941f8151327d1090bfbbaba01b4bc1a849
isma <ismajesuis@live.fr>
2016-02-23 18:48:23 +0100

    Merge de la classe Pawn avec la classe Player

--- src/com/umons/model/Player.java
+++ src/com/umons/model/Player.java
@@ -1,22 +1,41 @@
 package com.umons.model;
 
-public class Player {
+public class Player{
 	//faire hériter de Rules
+	
+	//Mur
 	private int numberOfWall;
-	private Wall wall;
-	private Pawn pion;
-	//tous les paramètres grid sont temporaires car la classe va heriter de grid
+	//Pion
+	private int posY;
+	private int posX;
 	
 	public Player(int number, Grid grid) {
 		/**
+		 *Initialise les murs, le pion (1 ou 2) et la connection avec les cases
 		 * @param number est le "numero" du joueur (joueur 1 ou 2)
 		 **/
 		numberOfWall = 10;
-		pion = new Pawn(number, grid);
-		wall = new Wall();
+		//initialisation du pion
+		posX = 8;
+		if (number == 1) {
+			posY = 16;
+		}else if (number == 2){
+			posY = 0;
+		}else {
+			//Si pas 1 ou 2, alors on le place au mileu
+			posY = 8;
+		}
+		//connection avec la case
+		//Rempli la case du joueur 2
+		grid.setItemInGrid(0, 8, true);
+		//Rempli la case du joueur 1
+		grid.setItemInGrid(16, 8, true);
 	}
 	
 	public void putWall(Grid grid, String position, int x, int y){
+		/**
+		 * Pose un mur sur la grille en remplissant un les item de type 2
+		 */
 		if (position.equals("horizontal")){
 			//appele aux verif de Rules
 			for (int j = y; j < y + 3; j++){
@@ -30,13 +49,57 @@ public class Player {
 		}
 	}
 	
-	public Pawn getPawn() {
+	public boolean move(String direction, Grid grid) {
 		/**
-		 * @return le pion associe aux joueur
+		 * Mutateur de Pawn, deplace pion.
+		 * @param bouge le pion d'une case en fonction de la direction
+		 * @param ouest, est, nord, sud
 		 */
-		return pion;
+		//On verra ça plus tard, en gros, ce sont des enchainements de else if plus lisible
+		switch (direction) {
+		//permet d'enlever de la grille le pion et de le remmettre sur les nouvelles coordonees
+		case "nord":
+			if (Rules.rMove(this.posX, posY)) { //ici c'est x, y car la methode s occupe de l inversement ligne colonne
+				grid.setItemInGrid(this.posY, this.posX, false);
+				this.posY -= 2;
+				grid.setItemInGrid(this.posY, this.posX, true);
+				return true;
+			}else {
+				return false;
+			}
+		case "ouest":
+			if (Rules.rMove(this.posX, posY)){
+				grid.setItemInGrid(this.posY, this.posX, false);
+				this.posX -= 2;
+				grid.setItemInGrid(this.posY, this.posX, true);
+				return true;
+			}else {
+				return false;
+			}
+		case "est":
+			if (Rules.rMove(this.posX, posY)) {
+				grid.setItemInGrid(this.posY, this.posX, false);
+				this.posX += 2;
+				grid.setItemInGrid(this.posY, this.posX, true);
+				return true;
+			}else {
+				return false;
+			}
+		case "sud":
+			if (Rules.rMove(this.posX, posY)) {
+				grid.setItemInGrid(this.posY, this.posX, false);
+				this.posY += 2;
+				grid.setItemInGrid(this.posY, this.posX, true);
+				return true;
+			}else {
+				return false;
+			}
+		}
+		//si le gars n a pas saisi la bonne direction
+		return false;
 	}
 	
+	//Accesseur des murs
 	public int getNumberOfWall() {
 		/**
 		 * @return le nombre de mur du joueur de type int
@@ -44,5 +107,21 @@ public class Player {
 		return numberOfWall;
 	}
 	
+	//Accesseur du pion
+	public int getY() {
+		/**
+		 * Accesseur
+		 * @return la position en y du Pion (LIGNE)
+		 */
+		return posY;	
+	}
+	
+	public int getX(){
+		/**
+		 * Accesseur
+		 * @return la position du Pion (COLONNE)
+		 */
+		return posX;
+	}
 	
 }
